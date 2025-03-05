f11c89c73174eee0e583f9451b9f4d64817c1ae3
Cario <domisunsay@gmail.com>
2025-03-05 12:09:31 +0100

    Update Movement

---
+++ Scripts/Player.cs
@@ -0,0 +1,62 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	private Node3D _head;
+	private Camera3D _camera;
+
+	public override void _Ready()
+	{
+		
+		_head = GetNode<Node3D>("Head");
+		_camera = GetNode<Camera3D>("Head/Camera");
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if(@event is InputEventMouseMotion m)
+		{
+			_head.RotateY(-m.Relative.X * CamSenstitivity);
+		}
+	}
+
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("space") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
